ab7509148f4f1b31e0da348c2357583462f12e5a
Brannon Wadforth <whiskey@digitalnode.io>
2019-07-14 17:41:45 +1000

    Fixing cors

--- src/fairings/cors.rs
+++ src/fairings/cors.rs
@@ -14,6 +14,7 @@ impl Fairing for Cors {
 
     fn on_response(&self, request: &Request, response: &mut Response) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:3000"));
+        response.set_header(Header::new("Access-Control-Allow-Origin", "https://bmwadforth.com"));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
