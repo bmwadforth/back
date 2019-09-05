df5a116342d79244f6d4a950177e917b8a9d97d8
Brannon Wadforth <whiskey@digitalnode.io>
2019-09-05 21:16:22 +1000

    Adding CORS

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"bmwadforth/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://bmwadforth.com", "http://bmwadforth.com", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge: 24 * time.Hour,
+	}))
+
 	r.GET("/ping",  controllers.Ping)
 	api := r.Group("/api")
 	api.GET("/articles", controllers.GetArticles)
