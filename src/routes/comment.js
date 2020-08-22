4f3b5b2667d68a9e65ecb7c054338c80c2c2b0fd
Brannon Wadforth <whiskey@digitalnode.io>
2020-08-22 22:07:49 +1000

    Updating logic

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import buildResponse from '../util/response';
-import { Comment } from '../data';
+import { Article, Comment } from '../data';
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  const comments = await Comment.findAll();
-  res.json(buildResponse('Fetched articles successfully', comments));
+  try {
+    const comments = await Comment.findAll();
+    res.json(buildResponse('Fetched comments successfully', comments));
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.put('/', async (req, res, next) => {
   try {
     const { comment } = req.body;
     const { article } = req.query;
-    // TODO: Validate request body
     await Comment.create({ ip: req.connection.remoteAddress, comment, articleId: article });
     res.json(buildResponse('Comment created successfully', null));
   } catch (err) {
