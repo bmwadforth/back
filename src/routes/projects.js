57d84ff9f10f0fcf270f9e7cef016056a80e33d4
Brannon Wadforth <whiskey@digitalnode.io>
2020-08-28 17:05:30 +1000

    Adding github meta to projects

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,14 +1,34 @@
 import express from 'express';
 import buildResponse from '../util/response';
-import { Article, Comment, Project } from '../data';
-import { authorize } from '../middleware/authorization';
+import { Project } from '../data';
+import axios from 'axios';
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
+    const projectsToReturn = [];
     const projects = await Project.findAll({});
-    res.json(buildResponse('Fetched projects successfully', projects));
+    for (let project of projects) {
+      project = project.toJSON();
+      //["https:/", "/bmwadforth/<repo>"]
+      const repoPath = project.repository.split('/github.com');
+      const res = await axios.get(`https://api.github.com/repos${repoPath[1]}/commits/master`, {
+        auth: {
+          username: 'bmwadforth',
+          password: process.env.BMWADFORTH_GITHUB_TOKEN,
+        },
+      });
+
+      const data = res.data;
+      project.githubMeta = {
+        hash: data.sha,
+        stats: data.stats,
+      };
+
+      projectsToReturn.push(project);
+    }
+    res.json(buildResponse('Fetched projects successfully', projectsToReturn));
   } catch (err) {
     next(err);
   }
