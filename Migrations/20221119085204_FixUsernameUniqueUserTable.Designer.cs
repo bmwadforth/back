2cc72bb8330dbbb958d596e9831093de5d8b0cde
Brannon Wadforth <whiskey@digitalnode.io>
2022-11-19 20:02:55 +1100

    Adding migration to make sure username is unique

--- Migrations/20221119085204_FixUsernameUniqueUserTable.Designer.cs
+++ Migrations/20221119085204_FixUsernameUniqueUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bmwadforth.Common.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bmwadforth.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20221119085204_FixUsernameUniqueUserTable")]
+    partial class FixUsernameUniqueUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
